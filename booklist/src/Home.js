cfc6f4f03336032e6de23d7805eb0d0512d9a414
nickfff-dev <hunchographics@gmail.com>
2022-08-06 23:43:35 +0300

    ss

---
+++ booklist/src/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+
+function Home() {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Welcome to the Booklist</h1>
+                    <p>This is a simple booklist app. It allows you to add, edit, and delete books.</p>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+
+export default Home;
